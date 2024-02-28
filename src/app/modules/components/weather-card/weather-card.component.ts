91fabaa3dcbb1d1e0d4bf2213f0d8acfe7a4ec47
leafgg <jobs.lugui@gmail.com>
2024-02-27 21:50:40 -0300

    feat: consuming api and generate card with infos

---
+++ src/app/modules/components/weather-card/weather-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { IWeatherData } from 'src/app/models/interfaces/IWeatherData';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: ['./weather-card.component.scss']
+})
+export class WeatherCardComponent {
+  // wheater prevision received from dad component
+  @Input() weatherDataInput!: IWeatherData;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
+}
---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apikey = 'e05ad2954bf752151bb83259221d5f00';
+
+  constructor(private http: HttpClient ) {}
+
+  getWeatherData(cityName: string): Observable<any>{
+
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apikey}`,
+    {})
+
+  }
+}
